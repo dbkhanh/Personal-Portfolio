dd13b7c3c87648f84a269281b1fc6666ad1bc9cc
dbkhanh <db2.khanh@gmail.com>
2024-09-01 13:20:32 +0700

    restructure

--- back-end.js
+++ back-end.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Serve static files from the 'html' folder
 app.use(express.static(path.join(__dirname, 'html')));
 
-// Route handler for the homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'html', 'home.html'));
 });
